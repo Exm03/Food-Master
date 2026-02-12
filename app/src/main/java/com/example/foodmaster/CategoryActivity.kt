ed27b6ac8f71c9561abcc3f84893a53f9426596b
Yan Lagun <exm230600@gmail.com>
2026-02-13 00:15:36 +0100

    Add design for first page and add logic for button

--- app/src/main/java/com/example/foodmaster/CategoryActivity.kt
+++ app/src/main/java/com/example/foodmaster/CategoryActivity.kt
@@ -1,32 +1,20 @@
 package com.example.foodmaster
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class CategoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_category)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-
     }
-
-    fun btnClick(view: View) {
-        val intent = Intent(this, CategoryActivity::class.java)
-        startActivity(intent)
-    }
-
-
-
 }
