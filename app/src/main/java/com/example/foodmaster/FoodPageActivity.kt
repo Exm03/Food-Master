417bb84f4f60f39d623e9d639268ad71aae76164
Yan Lagun <exm230600@gmail.com>
2026-02-13 11:47:25 +0100

    Add FoodPage and add notifications

--- app/src/main/java/com/example/foodmaster/FoodPageActivity.kt
+++ app/src/main/java/com/example/foodmaster/FoodPageActivity.kt
@@ -1,12 +1,36 @@
 package com.example.foodmaster
 
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
 
 class FoodPageActivity : AppCompatActivity() {
+
+    companion object {
+        val CHANNEL_ID = "firstchannel"
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +40,62 @@ class FoodPageActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
+
+        val image: ImageView = findViewById(R.id.foodPageImage)
+        val title: TextView = findViewById(R.id.foodPageTitle)
+        val text: TextView = findViewById(R.id.foodPageText)
+        val btn: Button = findViewById(R.id.btnAddToBasket)
+
+
+        image.setImageResource(intent.getIntExtra("foodImage", 0))
+        title.text = intent.getStringExtra("foodTitle")
+        text.text = intent.getStringExtra("foodText")
+
+        createNotificationChannel()
+        btn.setOnClickListener {
+
+            val intent = Intent(this, CategoryActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
+            val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+            val timeId = SimpleDateFormat("ddHHmmss", Locale.GERMANY).format(Date()).toInt()
+            var builder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.carts_1)
+                .setContentTitle("Товар добавлен в корзину")
+                .setContentText("Вы добавили ${title.text}")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+
+            val  manager = NotificationManagerCompat.from(this)
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                manager.notify(timeId, builder.build())
+            }
+        }}
+
+        private fun createNotificationChannel() {
+            // Create the NotificationChannel, but only on API 26+ because
+            // the NotificationChannel class is not in the Support Library.
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val name = getString(R.string.app_log)
+                val descriptionText = getString(R.string.app_desc)
+                val importance = NotificationManager.IMPORTANCE_DEFAULT
+                val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                    description = descriptionText
+                }
+                // Register the channel with the system.
+                val notificationManager: NotificationManager =
+                    getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                notificationManager.createNotificationChannel(channel)
+            }
+        }
+
+
+
+
+
 }
--- app/src/main/java/com/example/foodmaster/adapters/FoodAdapter.kt
+++ app/src/main/java/com/example/foodmaster/adapters/FoodAdapter.kt
@@ -2,12 +2,15 @@ package com.example.foodmaster.adapters
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.foodmaster.FoodPageActivity
 import com.example.foodmaster.R
 import com.example.foodmaster.models.Food
 
@@ -33,6 +36,18 @@ class FoodAdapter(private var dishes: List<Food>, private var context: Context):
         holder.title.text = dishes[position].title
         holder.anons.text = dishes[position].anons
         holder.image.setImageResource(imageId)
+
+        holder.btn.setOnClickListener {
+         val intent = Intent(context, FoodPageActivity::class.java)
+
+            intent.putExtra("foodImage", imageId)
+            intent.putExtra("foodTitle", dishes[position].title)
+            intent.putExtra("foodText", dishes[position].description)
+
+
+            context.startActivity(intent)
+
+        }
     }
 
     override fun getItemCount(): Int {
@@ -43,6 +58,8 @@ class FoodAdapter(private var dishes: List<Food>, private var context: Context):
         val image: ImageView = view.findViewById(R.id.foodImage)
         val title: TextView = view.findViewById(R.id.foodTitle)
         val anons: TextView = view.findViewById(R.id.foodAnons)
+
+        val btn: Button = view.findViewById(R.id.foodBtn)
 
     }
 }
